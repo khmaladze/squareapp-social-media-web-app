d2a65ec7705b1fb38c62cb1cf21b0c1a7eb494fd
“khmaladze” <khmaladze.g.7@gmail.com>
2022-03-15 15:47:57 +0400

    add user register

---
+++ backend/routes/auth.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Joi = require("joi");
+
+// check if email is valid
+function isValidEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email.toLowerCase()
+  );
+}
+
+// check if email clients are gmail or yahoo
+function isValidGmailProvider(email) {
+  return /.+@(gmail)\.com$/.test(email.toLowerCase());
+}
+
+// validation request schema
+const requestSchema = Joi.object({
+  firstName: Joi.string()
+    .pattern(/^[a-z]+$/)
+    .lowercase()
+    .min(2)
+    .max(50)
+    .trim()
+    .required(),
+  lastName: Joi.string()
+    .pattern(/^[a-z]+$/)
+    .lowercase()
+    .min(2)
+    .max(50)
+    .trim()
+    .required(),
+  userName: Joi.string().min(2).max(30).trim().required(),
+  birthDate: Joi.string().isoDate().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().lowercase().min(2).max(30).trim().required(),
+  confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+});
+
+///////////////////////////
+// /* Register user  */  //
+///////////////////////////
+router.post("/register", async (req, res) => {
+  try {
+    // get data from body
+    let {
+      firstName,
+      lastName,
+      userName,
+      birthDate,
+      email,
+      password,
+      confirmPassword,
+    } = req.body;
+
+    // check if all fields isn't empty
+    if (
+      !firstName ||
+      !lastName ||
+      !userName ||
+      !birthDate ||
+      !email ||
+      !password ||
+      !confirmPassword
+    ) {
+      res
+        .status(400)
+        .json({ success: false, message: "please add all the field" });
+    }
+
+    // validate request body
+    const schemaValidation = await requestSchema.validateAsync(req.body);
+
+    // validate email provider
+    if (!isValidEmail(email)) {
+      res
+        .status(400)
+        .json({ success: false, message: "only valid email is allowed" });
+    } else if (!isValidGmailProvider(email)) {
+      res
+        .status(400)
+        .json({ success: false, message: "only @gmail.com is allowed" });
+    }
+
+    let userAlreadyExists = User.findOne({ email: email });
+
+    console.log(userAlreadyExists);
+
+    if (!userAlreadyExists) {
+      const user = await User.create({
+        firstName: firstName,
+        lastName: lastName,
+        userName: userName,
+        birthDate: birthDate,
+        email: email,
+        password: password,
+      });
+      res
+        .status(200)
+        .json({ success: true, message: "user register successfully.", user });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "you can't register with this email",
+      });
+    }
+  } catch (error) {
+    res
+      .status(400)
+      .json({ success: false, message: "error, please try later" });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-
-connectDB();
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+
+// Middleware
+app.use(bodyParser.json());
+
+connectDB();
+
+// Models
+require("./models/userModel");
+
+// Routes
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
