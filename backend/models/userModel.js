d238c92cc179519f36eb6b2ea039d3747495d387
“khmaladze” <khmaladze.g.7@gmail.com>
2022-03-15 15:47:31 +0400

    change user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,40 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    biography: {
+      type: String,
+      required: false,
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    isBlocked: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-  },
-  biography: {
-    type: String,
-    required: true,
-  },
-  birth_date: {
-    type: Date,
-    required: true,
-  },
-  is_blocked: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
