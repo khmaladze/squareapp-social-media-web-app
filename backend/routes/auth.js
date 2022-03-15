adb7a4d0917c472e94ef3481c5eff76f21d0123f
“khmaladze” <khmaladze.g.7@gmail.com>
2022-03-15 23:59:08 +0400

    add user login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -19,7 +19,7 @@ function isValidGmailProvider(email) {
   return /.+@(gmail)\.com$/.test(email.toLowerCase());
 }
 
-// validation request schema
+// validation register request schema
 const registerRequestSchema = Joi.object({
   firstName: Joi.string()
     .pattern(/^[a-z]+$/)
@@ -41,6 +41,19 @@ const registerRequestSchema = Joi.object({
   password: Joi.string().lowercase().min(2).max(30).trim().required(),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
 });
+
+// validation login request schema
+const loginRequestSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().lowercase().min(2).max(30).trim().required(),
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 ///////////////////////////
 // /* Register User  */  //
@@ -139,7 +152,46 @@ router.post(
   "/login",
   asyncHandler(async (req, res) => {
     try {
-    } catch (error) {}
+      // get data from body
+      let { email, password } = req.body;
+
+      // check if all fields isn't empty
+      if (!email || !password) {
+        res
+          .status(400)
+          .json({ success: false, message: "please add all the field" });
+      }
+
+      // validate request body
+      const schemaValidation = await loginRequestSchema.validateAsync(req.body);
+
+      // validate email provider
+      if (!isValidEmail(email)) {
+        res
+          .status(400)
+          .json({ success: false, message: "only valid email is allowed" });
+      } else if (!isValidGmailProvider(email)) {
+        res
+          .status(400)
+          .json({ success: false, message: "only @gmail.com is allowed" });
+      }
+
+      // Check for user email
+      const user = await User.findOne({ email });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+          user,
+          token: generateToken(user._id),
+        });
+      } else {
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(400)
+        .json({ success: false, message: "error, please try later" });
+    }
   })
 );
 
