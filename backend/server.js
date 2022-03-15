79fecfc89b7988f9941b1dc97ed91b0db79a7ed1
“khmaladze” <giorgi.k@cybx.io>
2022-03-15 10:47:17 +0400

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
