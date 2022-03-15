8ead317d4a4d3045499cfa725482f88c2b70b787
“khmaladze” <khmaladze.g.7@gmail.com>
2022-03-15 11:06:35 +0400

    add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  biography: {
+    type: String,
+    required: true,
+  },
+  birth_date: {
+    type: Date,
+    required: true,
+  },
+  is_blocked: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
