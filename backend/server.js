e5c9ef8ca548b46202fa2827a302ca5b0b84b520
khmaladze <khmaladze.g.7@gmail.com>
2022-03-16 16:39:00 +0400

    add isValidEnv function

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,19 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
+const isValidEnv = () => {
+  if (process.env.PORT && process.env.JWT_SECRET && process.env.MONGO_URI) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 // Middleware
 app.use(bodyParser.json());
-
-connectDB();
+if (isValidEnv()) {
+  connectDB();
+}
 
 // Models
 require("./models/userModel");
@@ -19,6 +28,21 @@ require("./models/userModel");
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+if (isValidEnv()) {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+} else {
+  console.log("error please add .env file to run this program💻💻💻");
+  console.log(
+    JSON.stringify(
+      {
+        PORT: "PORT number",
+        JWT_SECRET: "your jwt secret",
+        MONGO_URI: "your mongodb uri",
+      },
+      null,
+      2
+    )
+  );
+}
