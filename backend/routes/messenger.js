9b6a73fc88a65bcaf6f5deb1d171eb105117485b
khmaladze <khmaladze.g.7@gmail.com>
2022-03-18 13:55:24 +0400

    add message send

--- backend/routes/messenger.js
+++ backend/routes/messenger.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Conversation = require("../models/conversationModel");
-const Messsage = require("../models/messageModel");
+const Message = require("../models/messageModel");
 const asyncHandler = require("express-async-handler");
 const Joi = require("joi");
 const { protect } = require("../middleware/requireUserLogin");
+
+// validation conversation request schema
+const conversationRequestSchema = Joi.object({
+  first: Joi.string().trim().required(),
+  receiver: Joi.string().trim().required(),
+});
+
+// validation message request schema
+const messageRequestSchema = Joi.object({
+  text: Joi.string().min(1).max(300).trim().required(),
+});
 
 /////////////////////////////////
 //  /* Create Conversation */  //
@@ -15,6 +26,8 @@ router.post(
   asyncHandler(async (req, res) => {
     try {
       const { first, receiver } = req.body;
+
+      const validate = await conversationRequestSchema.validateAsync(req.body);
 
       if (first && receiver) {
         // check if conversation already created
@@ -45,10 +58,18 @@ router.post(
         });
       }
     } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: "try later",
-      });
+      if (error.details) {
+        if (error.details[0].message) {
+          res
+            .status(400)
+            .json({ success: false, message: error.details[0].message });
+        }
+      } else {
+        res.status(500).json({
+          success: false,
+          message: "try later",
+        });
+      }
     }
   })
 );
@@ -117,6 +138,47 @@ router.get(
         success: false,
         message: "try later",
       });
+    }
+  })
+);
+
+/////////////////////////////////
+//     /* Send Message */      //
+/////////////////////////////////
+router.post(
+  "/send/message/:conversationId",
+  protect,
+  asyncHandler(async (req, res) => {
+    try {
+      const { text } = req.body;
+
+      const validate = await messageRequestSchema.validateAsync(req.body);
+
+      const addMessage = await Message.create({
+        conversationId: req.params.conversationId,
+        sender: req.params.id,
+        text,
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "message send successfully",
+        addMessage,
+      });
+    } catch (error) {
+      console.log(error);
+      if (error.details) {
+        if (error.details[0].message) {
+          res
+            .status(400)
+            .json({ success: false, message: error.details[0].message });
+        }
+      } else {
+        res.status(500).json({
+          success: false,
+          message: "try later",
+        });
+      }
     }
   })
 );
