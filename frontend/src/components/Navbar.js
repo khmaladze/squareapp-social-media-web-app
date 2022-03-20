6610626c5273f4a9f2e236d2ba5355c86b202730
khmaladze <khmaladze.g.7@gmail.com>
2022-03-20 15:41:40 +0400

    add components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { Link, useNavigate } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useDispatch } from "react-redux";
+import { clear } from "../features/auth";
+
+export const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSignout = () => {
+    localStorage.removeItem("user");
+    dispatch(clear());
+    setAnchorEl(null);
+    window.location.reload();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <Main>
+        <Link to="/">
+          <img
+            style={{ height: "50px", cursor: "pointer" }}
+            src="/assets/logo.png"
+            alt="images"
+          />
+        </Link>
+        <>
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle style={{ fontSize: "35px", color: "#1238fd" }} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <Link to="/profile">
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+              </Link>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleSignout} style={{ color: "red" }}>
+                <LogoutIcon />
+                Log Out
+              </MenuItem>
+            </Menu>
+          </div>
+        </>
+      </Main>
+    </>
+  );
+};
+
+const Main = styled.div`
+  position: fixed;
+  background: #ffffff;
+  box-shadow: rgb(60 64 67 / 30%) 0px 1px 2px 0px,
+    rgb(60 64 67 / 15%) 0px 2px 6px 2px;
+  padding: 25px;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #f9f9f9;
+`;
