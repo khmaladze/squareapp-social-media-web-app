2bdb59578b7d96a6ccc811a51b114f74e59a901c
khmaladze <khmaladze.g.7@gmail.com>
2022-03-21 21:34:23 +0400

    add storie schema

---
+++ backend/models/storieModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const storieSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    video: {
+      type: String,
+      trim: true,
+    },
+    privacy: {
+      type: String,
+      enum: ["onlyMe", "friends", "public"],
+      default: "onlyMe",
+    },
+    comment: [
+      {
+        comment: String,
+        commentBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "User",
+        },
+        date: { type: Date, default: Date.now },
+      },
+    ],
+    like: [
+      {
+        like: {
+          type: String,
+          enum: ["like", "love"],
+          default: "like",
+        },
+        likeBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "User",
+        },
+        date: { type: Date, default: Date.now },
+      },
+    ],
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Storie", storieSchema);
