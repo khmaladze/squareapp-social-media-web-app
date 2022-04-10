13b967a031bcd6328c6e4515ed4d21a8c9aec896
khmaladze <khmaladze.g.7@gmail.com>
2022-04-10 08:00:55 +0400

    add main navbar

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import MainNavBar from "../components/main/MainNavBar";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <MainPage>
+      <MainNavBar />
+    </MainPage>
+  );
 };
+
+const MainPage = styled.div`
+  height: 100%;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 export default Main;
