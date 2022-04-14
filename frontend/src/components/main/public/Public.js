d0bd818f091fc6a8cad4efddd279d397237a1a12
khmaladze <khmaladze.g.7@gmail.com>
2022-04-14 17:02:23 +0400

    add post component

--- frontend/src/components/main/public/Public.js
+++ frontend/src/components/main/public/Public.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Post from "./Post";
 import Storie from "./Storie";
 
 const Public = () => {
   return (
     <>
       <Storie />
+      <Post />
     </>
   );
 };
