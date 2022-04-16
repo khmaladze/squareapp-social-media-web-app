8b018c32abc3bde05f473e0393d528bc179afc72
khmaladze <khmaladze.g.7@gmail.com>
2022-04-16 11:30:13 +0400

    add remove friend request

--- backend/routes/friend.js
+++ backend/routes/friend.js
@@ -150,4 +150,74 @@ router.post(
   })
 );
 
+/////////////////////////////////
+// /* Delete Friend Request */ //
+/////////////////////////////////
+router.post(
+  "/remove",
+  protect,
+  asyncHandler(async (req, res) => {
+    try {
+      const { reciver } = req.body;
+      console.log(reciver);
+      console.log("reciver", reciver);
+      const friendsData = await User.find({
+        _id: { $in: req.user.friends.map((id) => reciver) },
+        isBlocked: false,
+      }).select(
+        "_id profileImage backgroundImage userName firstName lastName place hobby"
+      );
+      console.log(friendsData);
+      //   if (friendsData.length > 0) {
+      //     res.status(400).json({
+      //       success: false,
+      //       message: "can't add friend",
+      //     });
+      //   }
+      if (friendsData.length > 0) {
+        res.status(400).json({
+          success: false,
+          message: "user is your friend",
+        });
+      }
+      if (friendsData.length == 0) {
+        const alreadySend = await Friend.find({
+          sender: req.user._id,
+          reciver: reciver,
+        });
+        console.log(alreadySend);
+
+        if (alreadySend.length > 0) {
+          await Friend.deleteOne({
+            sender: req.user._id,
+            reciver: reciver,
+          });
+          res.status(200).json({
+            success: true,
+            message: "friend request remove",
+          });
+        }
+
+        if (alreadySend.length == 0) {
+          const friendAdd = await Friend.create({
+            sender: req.user,
+            reciver: reciver,
+          });
+
+          res.status(200).json({
+            success: true,
+            message: "friend request send successfully",
+            friendAdd,
+          });
+        }
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "try later",
+      });
+    }
+  })
+);
+
 module.exports = router;
